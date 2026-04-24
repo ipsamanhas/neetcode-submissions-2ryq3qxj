76f3f09b1cb0411db19e29111617483c8c904bc4
Ipsa Manhas <imanhas@ualberta.ca>
2026-04-23 19:57:20 -0600

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # return the k most freq elements
+        # order can be anything
+
+        # we will use BUCKET SORT
+
+        count = {} # a hashmap that counts the occurrence of each element
+        freq = [] # an array which has the same number of arrays 
+        #within it, as the length of the input array 
+        for i in range(len(nums)+1):
+            freq.append([])
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        
+        for n,c in count.items():
+            freq[c].append(n) # n occurs c number of times
+
+        result = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
+
+
