5b031de80395ad20b2482eba9d1c32ea99208a1c
Ipsa Manhas <imanhas@ualberta.ca>
2026-05-12 21:54:12 -0600

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        # i can use a for loop to go thru the array and add each element of that array to a new one, then i can compare if an element is present in the array alr
+
+        result = []
+
+        for num in nums:
+            if num not in result:
+                result.append(num)
+            else:
+                return True
+
+        return False        
+
