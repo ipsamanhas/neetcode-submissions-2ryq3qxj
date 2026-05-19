902e56ff55279dbee7f6a68bd72a07dfd0a4c7c2
Ipsa Manhas <imanhas@ualberta.ca>
2026-05-19 11:04:45 -0600

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        # we can only sell the coin after we buy it
+        # need to buy a neetcoin at a low price
+        # sell the neetcoin at a high price to make profit
+
+        left = 0
+        right = 1
+
+        profit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                diff = prices[right] - prices[left]
+                profit = max(profit, diff)
+            else:
+                left = right
+            right += 1
+
+        return profit
